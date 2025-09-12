0abf37327e56323ad672159720ae496bf7530f3f
Harold Gómez <haroldg@gmail.com>
2025-09-12 19:04:59 -0300

    updating credentials

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ st.set_page_config(
 # LÓGICA DE ACCESO A MÚLTIPLES CUENTAS (CROSS-ACCOUNT)
 # ========================================================================
 
-@st.cache_data(ttl=900) # Cachea las credenciales temporales por 15 minutos
+@st.cache_resource(ttl=900) # Cachea las credenciales temporales por 15 minutos
 def get_cross_account_boto3_client(service_name: str):
     """
     Asume el rol de la cuenta cliente y retorna un cliente de boto3 para el servicio especificado.
